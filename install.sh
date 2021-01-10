e7b3110ad89205e1b20e04a3019943a5fc639037
root <root@test.icsr.wiki>
2021-01-10 08:18:19 +0000

    fix install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,6 @@ echo "export DISPLAY=:0" >> /etc/profile
 
 apt install -y openbox obmenu obconf vnc4server xfce4-panel pcmanfm ttf-wqy-microhei dbus-x11 lxterminal gpicview
 
-cp ./.gtkrc-2.0 /root/.gtkrc-2.0
+cp ./gtkrc-2.0 /root/.gtkrc-2.0
 
 echo "done"
